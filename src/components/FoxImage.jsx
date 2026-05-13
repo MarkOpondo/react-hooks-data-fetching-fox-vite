5d0be11a1daf46b84c4030b921651834fc8169bd
Mark Opondo <markopondoi.t@gmail.com>
2026-05-13 09:54:47 +0300

    Fetch fox image on mount usin g

--- src/components/FoxImage.jsx
+++ src/components/FoxImage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import foxLogo from '../assets/fox-logo.png'
 
 const API_URL = "https://randomfox.ca/floof/"
@@ -6,6 +6,34 @@ const API_URL = "https://randomfox.ca/floof/"
 function FoxImage() {
   const [image, setImage] = useState(foxLogo)
   
+  useEffect(() => {
+    const controller = new AbortController()
+
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(API_URL, { signal: controller.signal})
+        
+        if (!response.ok) {
+          throw new Error("Failed to fetch image")
+        }
+        
+        const data = await response.json()
+        console.log(data);
+        setImage(data.image)
+      
+      } catch(error) {
+        
+        if ( error.name === "AbortError") return
+        console.log(error);
+      
+      }
+    }
+
+    fetchImage() // execute the function
+
+    return () => controller.abort() //clean up on unmount
+  }, [])
+
   return (
     <div>
       <p>Learn more about us!</p>
